d67a350cedf7040a75b8a7f231ec0a5fe6f1351e
anton <a.solovjev@vzv.su>
2024-02-02 15:22:12 +0300

    conclusion of the obstacles

--- fly_garbage_animation.py
+++ fly_garbage_animation.py
@@ -3,50 +3,42 @@ import random
 
 from async_sleep import sleep
 from game_animation import draw_frame, get_frame_size
-from obstacles import Obstacle
-
+from obstacles import Obstacle, show_obstacles
 
 obstacles = []
 
 
-async def fly_garbage(canvas, column, garbage_frame, speed=0.5):
-    global obstacles
+async def fly_garbage(canvas, column, garbage_frame, coroutines, speed=0.5):
     """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
     rows_number, columns_number = canvas.getmaxyx()
+    frame_rows, frame_columns = get_frame_size(garbage_frame)
 
-    column = max(column, 0)
-    column = min(column, columns_number - 1)
+    column = max(column, 2)
+    column = min(column, columns_number - frame_columns - 1)
 
     row = 0
+    global obstacles
 
-    obstacle = Obstacle(row, column, get_frame_size(garbage_frame))
-    obstacles.append(obstacle)
+    new_obstacle = Obstacle(row, column, frame_rows, frame_columns)
+    obstacles.append(new_obstacle)
 
-    # while row < rows_number:
-    #     draw_frame(canvas, row, column, garbage_frame)
-    #     await sleep()
-    #     draw_frame(canvas, row, column, garbage_frame, negative=True)
-    #     row += speed
+    coroutine = show_obstacles(canvas, obstacles)
+    coroutines.append(coroutine)
 
     try:
-        while column > 0:
-            garbage_frame, row, col = obstacle.dump_bounding_box()
+        while row < rows_number:
+            # if new_obstacle in obstacles_in_last_collisions:
+            #     obstacles_in_last_collisions.remove(new_obstacle)
+            #     await explode(canvas, row + frame_rows // 2, column + frame_columns // 2)
+            #     return
 
             draw_frame(canvas, row, column, garbage_frame)
             await asyncio.sleep(0)
             draw_frame(canvas, row, column, garbage_frame, negative=True)
-            obstacle.column = column
             row += speed
-
-            #if obstacle in obstacles and obstacle.has_collision(spaceship_y, spaceship_x):
-                # Здесь можно добавить логику для обработки столкновения
-            #    pass
-
-            for obstacle in obstacles.copy():
-                if obstacle.column < 0:
-                    obstacles.remove(obstacle)
+            new_obstacle.row = row
     finally:
-        obstacles.remove(obstacle)
+        obstacles.remove(new_obstacle)
 
 
 async def fill_orbit_with_garbage(canvas, coord_x, coroutines, trash_frames):
@@ -54,7 +46,7 @@ async def fill_orbit_with_garbage(canvas, coord_x, coroutines, trash_frames):
         await sleep(tics=10)
         column = random.randint(1, coord_x - 1)
         garbage_frame = random.choice(trash_frames)
-        garbage_coroutine = fly_garbage(canvas, column, garbage_frame, speed=0.4)
+        garbage_coroutine = fly_garbage(canvas, column, garbage_frame, coroutines, speed=0.4)
         coroutines.append(garbage_coroutine)
         await sleep()
 
